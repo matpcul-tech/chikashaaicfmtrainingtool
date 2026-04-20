ddbbbda65068620479debc31294338fc1d7d274e
matpcul-tech <matpcul@gmail.com>
2026-04-20 18:19:48 -0500

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import CFMDataTool from './App'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <CFMDataTool />
+  </React.StrictMode>
+)
